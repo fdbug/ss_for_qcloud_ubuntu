274a5337d351471adbf1c17c24df7dcf354fa5a3
fdbug <fdbug@users.noreply.github.com>
2020-07-07 21:28:24 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # sudo su - root
 # wget https://raw.githubusercontent.com/fdbug/ss_for_qcloud_ubuntu/master/setup.sh
+sudo su - root
+cd ~
+wget https://raw.githubusercontent.com/fdbug/ss_for_qcloud_ubuntu/master/shadowsocks.json
+wget https://raw.githubusercontent.com/fdbug/ss_for_qcloud_ubuntu/master/shadowsocks.service
 # chmod x+r setup.sh
 sudo apt install python-pip
 pip install shadowsocks
