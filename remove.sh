33a5a7591384ab1bd4429bd36d085fa703cc0d63
fdbug <fdbug@users.noreply.github.com>
2020-07-07 21:57:39 +0800

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# need root 
+if ［ `whoami` = "root" ］
+then
+    echo "remove start!"
+else
+    echo "not root!"
+    exit 0
+fi
+pip uninstall shadowsocks
+rm /etc/shadowsocks.json
+systemctl disable /etc/systemd/system/shadowsocks.service
+rm /etc/systemd/system/shadowsocks.service
