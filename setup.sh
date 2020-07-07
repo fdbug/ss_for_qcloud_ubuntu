f5cbaebd0db89f3a42d5f8724960f7415f2957e0
fdbug <fdbug@users.noreply.github.com>
2020-07-07 21:36:44 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-# sudo su - root
+# need root 
 # wget https://raw.githubusercontent.com/fdbug/ss_for_qcloud_ubuntu/master/setup.sh
-sudo su - root
+if ［ `whoami` = "root" ］
+then
+    echo "start!"
+else
+    echo "not root!"
+    exit 0
+fi
 cd ~
 wget https://raw.githubusercontent.com/fdbug/ss_for_qcloud_ubuntu/master/shadowsocks.json
 wget https://raw.githubusercontent.com/fdbug/ss_for_qcloud_ubuntu/master/shadowsocks.service
 # chmod x+r setup.sh
-sudo apt install python-pip
+apt install python-pip
 pip install shadowsocks
-sudo cp ./shadowsocks.json /etc/shadowsocks.json
-sudo cp ./shadowsocks.service /etc/systemd/system/shadowsocks.service
+cp ./shadowsocks.json /etc/shadowsocks.json
+cp ./shadowsocks.service /etc/systemd/system/shadowsocks.service
 systemctl enable /etc/systemd/system/shadowsocks.service
 service shadowsocks restart
