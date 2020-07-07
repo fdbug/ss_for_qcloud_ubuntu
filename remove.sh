751e5c80a0792d59dec79593a0079e585ad0a420
fdbug <fdbug@users.noreply.github.com>
2020-07-07 22:23:15 +0800

    Update remove.sh
    fix bug

--- remove.sh
+++ remove.sh
@@ -7,7 +7,8 @@ else
     echo "not root!"
     exit 0
 fi
-pip uninstall shadowsocks
+ps aux | grep ssserver | grep -v grep  | awk 'system("kill -9 "$2)'
+echo y | pip uninstall shadowsocks
 rm /etc/shadowsocks.json
 systemctl disable shadowsocks
 rm /etc/systemd/system/shadowsocks.service
