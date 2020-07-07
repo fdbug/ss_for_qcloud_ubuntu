71246575465b032a8d0e647c26ea5b683b1ac2c1
fdbug <fdbug@users.noreply.github.com>
2020-07-07 21:55:55 +0800

    Update setup.sh
    fix bug

--- setup.sh
+++ setup.sh
@@ -16,5 +16,14 @@ apt install python-pip
 pip install shadowsocks
 cp ./shadowsocks.json /etc/shadowsocks.json
 cp ./shadowsocks.service /etc/systemd/system/shadowsocks.service
+# fix openssl bug
+sed -i 's/EVP_CIPHER_CTX_cleanup/EVP_CIPHER_CTX_reset/g' /usr/local/lib/python2.7/dist-packages/shadowsocks/crypto/openssl.py
 systemctl enable /etc/systemd/system/shadowsocks.service
 service shadowsocks restart
+# check
+if [ `netstat  -nap | grep 3389 | grep LISTEN|wc -l`=="1" ] 
+then
+    echo "ss install succeed!"
+else
+    echo "ss install failed! please manual check!"
+fi
